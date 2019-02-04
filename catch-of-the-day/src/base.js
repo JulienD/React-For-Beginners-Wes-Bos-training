0f8e93a88d791f41a8ddc8a4ad4b7a3949a0e693
Julien Dubreuil <dubreuil.julien@gmail.com>
2019-02-04 15:46:46 +0100

    Added default env file

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_WEATHER_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'catch-of-the-day-jdu.firebaseapp.com',
   databaseURL: 'https://catch-of-the-day-jdu.firebaseio.com'
 });
